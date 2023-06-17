4f6c44e5487c3954a1d7d0c94d37d93e3cb3d9f6
Yulia Kutova <kutova85@gmail.com>
2023-06-17 10:26:41 +0300

    add verificationToken

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,7 +1,9 @@
 const { User } = require("../../models/user");
-const { HttpError } = require("../../helpers");
+const { HttpError, sendEmail } = require("../../helpers");
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
+const { PROJECT_URL } = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -13,11 +15,23 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
+
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${PROJECT_URL}/api/users/verify/${verificationToken}">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
+
   res.status(201).json({
     user: {
       email: newUser.email,
